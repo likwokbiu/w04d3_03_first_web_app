8344d7ccfd55428a47e43a21eb366e50c297c806
likwokbiu <li_bill@ymail.com>
2018-02-14 16:35:12 -0500

    Completed on Feb 14

--- server.rb
+++ server.rb
@@ -12,8 +12,17 @@ get '/portfolio' do
   erb :gallery
 end
 
+get '/gallery' do
+  redirect to('/portfolio')
+end
+
 get '/about_me' do
   @skills = ['git', 'HTML', 'CSS', 'Ruby']
   @interests = ['cats', 'art', 'music', 'films', 'coffee']
   erb :about_me
+end
+
+get '/favourites' do
+  @fav_links = ['http://www.yahoo.ca', 'http://www.apple.ca', 'http://www.singtao.ca', 'http://www.youtube.ca', 'http://www.google.ca']
+  erb :favourites
 end
