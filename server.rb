9447ff7c17816c1deda3925f462d611217813abd
likwokbiu <li_bill@ymail.com>
2018-02-14 16:03:04 -0500

    2nd draft

--- server.rb
+++ server.rb
@@ -1,5 +1,19 @@
 require 'sinatra'
 
+get '/' do
+  redirect to('/home')
+end
+
 get '/home' do
   erb :index
+end
+
+get '/portfolio' do
+  erb :gallery
+end
+
+get '/about_me' do
+  @skills = ['git', 'HTML', 'CSS', 'Ruby']
+  @interests = ['cats', 'art', 'music', 'films', 'coffee']
+  erb :about_me
 end
